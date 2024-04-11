68003d971ad68abff6a9dc19fb1025ef115b9224
navigareweb <web@navigaresrl.com>
2024-04-11 12:23:25 +0200

    first commit

--- app/page.js
+++ app/page.js
@@ -1,94 +1,85 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./index.module.scss";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Image
+        src={"/400x600.jpeg"}
+        width={600}
+        height={600}
+        className={styles.img}
+        alt="imgLautrec"
+      ></Image>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
+        <h3 className={styles.text}>
+          MAURICE JOYANT AND THE POSTHUMOUS PRINTS
+        </h3>
+        <audio
+          controls
+          src="/MAURICE-JOYANT-AND-THE-POSTHUMOUS-PRINTS.mp3"
+          className={styles.audio}
+        >
+          Your browser does not support the
+          <code>audio</code> element.
+        </audio>
+        <p className={styles.p}>
+          Lautrec was a very prolific artist, despite his career made short by
+          his untimely death. His production includes 737 paintings, 4,748
+          drawings, 275 watercolors, 334 prints, and 33 posters. His tireless
+          need for production, together with a curiosity for formal
+          experimentation, led him to deal with graphic art, a form of art in
+          which he probably reached his highest peak. On the other hand,
+          Lautrec's years of activity were years of modernity and great
+          technological revolutions: the advent and spread of photography, the
+          improvements in printing techniques, including the introduction of
+          color lithography, were seductive elements for an artist who was among
+          the first to understand the importance of advertising. As Argan
+          recalls, “drawing posters or covers for a program represented for him
+          a real commitment, not less important than making a painting. Indeed,
+          in advertising, communication for spurring is more important than
+          representing. If representation is something fix that promises to be,
+          communication creeps in and strikes: for the first time, thanks to
+          Toulouse, artist's activity no longer aims to end up in a finished
+          object, that’s the painting, but unravels in an unstopped series of
+          paintings, engravings, drawings, even in a sketchbook leafed through
+          as if it were a collection of poems.” Even today Lautrec's works are
+          some of the most reproduced in the world, and are part of the
+          collective imagination associated with the Belle Époque. Nevertheless,
+          Toulouse was too modern to be understood by critics, and he did not
+          meet with the success he would have deserved; indeed, it can be said
+          that his fame was posthumous. The person who most managed to create
+          the myth of Lautrec was Maurice Joyant, a very close friend of the
+          artist, who first organized an exhibition of his works, and in 1922
+          persuaded Adéle, Lautrec's mother, to give the works in her possession
+          to the city of Albi, in order to establish the Musée Toulouse-Lautrec.
+          Joyant also published several Lautrec's works, making them accessible
+          to the general public. Together with the heirs of Adolphe Goupil – an
+          art dealer and the founder of Goupil & Cie, a company specializing in
+          the imprinting and publishing of original prints, which soon developed
+          a worldwide market for reproductions of works of art with branches in
+          London, Brussels, The Hague (where Vincent van Gogh also worked),
+          Berlin and Vienna, and New York – Joyant was committed in rearranging
+          and publishing Lautrec's graphic work after the artist's death. Some
+          of the works in the exhibition are part of these posthumous prints.
+          Among them, the Au Cirque series, which includes 39 drawings made
+          during Lautrec's stay in a sanatorium in 1899, which could only be
+          admired following the artist's exhibitions, when Maurice Joyant
+          reproduced 22 of them by using the photomechanical process in 1905 (a
+          second edition with the remaining drawings would appear in 1931).
+          Joyant, who had met Lautrec at school, when he was 8 years old,
+          started his career as a food critic before dealing with art in all of
+          its forms (he was a collector, publisher, gallery manager, and
+          critic). They shared an endless series of experiences and, among the
+          several passions, cooking was a common one. Few people know that
+          Lautrec, besides being a great gourmet, was a fine cook as well. After
+          his untimely death, Joyant collected and published the painter's
+          creative recipes, including that of the famous pigeons with olives:
+          this recipe was invented by Toulouse-Lautrec, who prepared it for
+          banquets he often laid out for his friends and favorite models. In the
+          book on display here, a 1966 edition, the recipes are illustrated by
+          the several drawings and menus specially created by the artist.
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
