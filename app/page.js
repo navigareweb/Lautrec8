567cd0ff4017d8fe92957288e38da0826cb71b5e
navigareweb <web@navigaresrl.com>
2024-09-04 16:32:43 +0200

    imgxf

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <Image
-        src={"/400x600.jpeg"}
+        src={"/Lautrec400x600.jpg"}
         width={600}
         height={600}
         className={styles.img}
